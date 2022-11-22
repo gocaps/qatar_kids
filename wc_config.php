47dfedac5831df59dc5c31a8c09b7eb618bef9f5
Dave <david.kennedy@ubc.ca>
2022-11-22 09:21:20 -0800

    denmark vs tunisia

--- wc_config.php
+++ wc_config.php
@@ -1,7 +1,7 @@
 <?php
 
 define('UPDATING', false);
-define('LAST_UPDATE', 'After Wales vs USA');
+define('LAST_UPDATE', 'After Denmark vs Tunisia');
 define('SHOW_TEAMS', true);
 define('SHOW_BONUS_TEXT', true);
 define('TWEET', false);
@@ -49,7 +49,7 @@ define('BELGIUM', 	serialize(array(BELGIUM_WINS, BELGIUM_TIES, BELGIUM_BONUS, BE
 
 /*************************************************/
 //D
-define('DENMARK_WINS', 0);		define('DENMARK_TIES', 0);		define('DENMARK_BONUS', 0);
+define('DENMARK_WINS', 0);		define('DENMARK_TIES', 1);		define('DENMARK_BONUS', 0);
 define('CROATIA_WINS', 0);		define('CROATIA_TIES', 0);		define('CROATIA_BONUS', 0);
 define('URUGUAY_WINS', 0);		define('URUGUAY_TIES', 0);		define('URUGUAY_BONUS', 0);
 define('DENMARK_OUT', 0);		define('CROATIA_OUT', 0);			define('URUGUAY_OUT', 0);
@@ -123,9 +123,9 @@ define('IRAN', 	serialize(array(IRAN_WINS, IRAN_TIES, IRAN_BONUS, IRAN_OUT)));
 
 /*************************************************/
 //J
-define('TUNISIA_WINS', 0);	define('TUNISIA_TIES', 0);	define('TUNISIA_BONUS', 0);
+define('TUNISIA_WINS', 0);	define('TUNISIA_TIES', 1);	define('TUNISIA_BONUS', 0);
 define('QATAR_WINS', 0);			define('QATAR_TIES', 0);			define('QATAR_BONUS', 0);
-define('SAUDI_WINS', 0);   define('SAUDI_TIES', 0);   define('SAUDI_BONUS', 0);
+define('SAUDI_WINS', 3);   define('SAUDI_TIES', 0);   define('SAUDI_BONUS', 0);
 define('COSTA_WINS', 0);		define('COSTA_TIES', 0);		define('COSTA_BONUS', 0);
 define('TUNISIA_OUT', 0);		define('QATAR_OUT', 0);			define('SAUDI_OUT', 0);		define('COSTA_OUT', 0);
 
@@ -143,7 +143,7 @@ define('COSTA',   serialize(array(COSTA_WINS, COSTA_TIES, COSTA_BONUS, COSTA_OUT
 /*************************************************/
 //K
 define('MBAPPE_GOALS', 0);    define('MBAPPE_BONUS', 0);
-define('MESSI_GOALS', 0);   define('MESSI_BONUS', 0);
+define('MESSI_GOALS', 2);   define('MESSI_BONUS', 0);
 define('KANE_GOALS', 0);   define('KANE_BONUS', 0);
 define('MBAPPE_OUT', 0);	 define('MESSI_OUT', 0); define('KANE_OUT', 0);
 
